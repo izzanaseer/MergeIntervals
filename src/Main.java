d07f35ce1d80b0231fc148ddc1cd6b0916d56191
izzanaseer <izzanaseer3@gmail.com>
2023-10-11 01:04:03 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.List;
+public class Main {
+    public void mergeIntervals(List<int[]> intervals){
+        if (intervals.size() <= 1){
+            return;
+        }
+    }
+    public static void main(String[] args) {
+    }
+}
